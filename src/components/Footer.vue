c2323fb6be8c7a0791b42225bffd7d81a40e1a36
LeoM <54435611+Tenlutshy@users.noreply.github.com>
2024-05-28 14:36:26 +0200

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="footer">
+
+        <ul class="link">
+            <li><IconTexteHover link="https://github.com/LeoMa33" icon="/icons/github.svg" text="LeoMa33"></IconTexteHover></li>
+            <li><IconTexteHover link="https://www.linkedin.com/in/léo-mace-142234266/" icon="/icons/linkedin.svg" text="Léo MACE"></IconTexteHover></li>
+            <li><IconTexteHover link="mailto:mace.leo33@gmail.com" icon="/icons/mail.svg" text="mace.leo33@gmail.com"></IconTexteHover></li>
+            <li><IconTexteHover link="tel:0682546806" icon="/icons/phone.png" text="06.82.54.68.06"></IconTexteHover></li>
+        </ul>
+
         <p>@Copyright, Tous droits réservés</p>
         <p>Développé par MACE Léo</p>
     </div>
 </template>
 
 <script setup lang="ts">
+
+import IconTexteHover from './IconTexteHover.vue'
 
 </script>
 
@@ -20,6 +30,19 @@
     flex-direction: column;
     justify-content: end;
     align-items: center;
+}
+
+.link {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 2dvw;
+    flex-grow: 1;
+}
+
+li img {
+    aspect-ratio: 1/1;
+    width: 2dvw;
 }
 
 .footer p {
---
+++ src/components/IconTexteHover.vue
@@ -0,0 +1,37 @@
+<template>
+    <a :href="props.link"><img :src="props.icon" alt=""><p>{{ props.text }}</p></a>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps(['link', 'icon', 'text']);
+
+</script>
+
+
+
+<style scoped>
+img {
+    aspect-ratio: 1/1;
+    width: 2dvw;
+}
+
+a {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    text-decoration: none;
+    color: white;
+    gap: 5px;
+    overflow: hidden;
+}
+
+a p {
+    width: 0px;
+}
+
+a:hover p {
+    width: auto;
+}
+
+</style>
